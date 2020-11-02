95bb3bd6e4e0f8353bf1739bc6e54fe9fe036e4b
BowieD <31174978+iBowie@users.noreply.github.com>
2020-11-02 17:27:32 +0500

    Update LockRotationRelative.cs

--- Assets/Scripts/LockRotationRelative.cs
+++ Assets/Scripts/LockRotationRelative.cs
@@ -1,15 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class LockRotationRelative : MonoBehaviour
 {
     public Transform target;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
