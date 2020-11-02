ab108d711146c94dd74d0f4dac88f6bf6bf95a48
BowieD <31174978+iBowie@users.noreply.github.com>
2020-11-02 20:11:50 +0500

    Update AttractedBy.cs

--- Assets/Scripts/AttractedBy.cs
+++ Assets/Scripts/AttractedBy.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class AttractedBy : MonoBehaviour
 {
     public static readonly List<AttractedBy> attracteds = new List<AttractedBy>();
 
+    [HideInInspector]
     public Rigidbody2D rigid;
     
     // Start is called before the first frame update
