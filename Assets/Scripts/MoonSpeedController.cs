93a91e1084db1185615fb3d53d918cef7e41a7f5
BowieD <31174978+iBowie@users.noreply.github.com>
2020-11-02 22:26:22 +0500

    Update MoonSpeedController.cs

--- Assets/Scripts/MoonSpeedController.cs
+++ Assets/Scripts/MoonSpeedController.cs
@@ -27,17 +27,6 @@ public class MoonSpeedController : MonoBehaviour
         toDisplay(currentSpeed, out float spdValue, out string spdUnits);
 
         UIText.text = $"Current Moon Speed: {spdValue:0.##} {spdUnits}/s\nTotal Distance: {disValue:0.##} {disUnits}";
-
-        var particleArray = new ParticleSystem.Particle[0];
-
-        int l = particles.GetParticles(particleArray);
-
-        for (int i = 0; i < particleArray.Length; i++)
-        {
-            ParticleSystem.Particle p = particleArray[i];
-
-            p.velocity = new Vector3(0, -currentSpeed);
-        }
     }
 
     void toDisplay(float value, out float visibleValue, out string units)
