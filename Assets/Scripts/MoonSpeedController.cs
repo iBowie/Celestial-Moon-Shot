b1ae4e8f572d981d3d0e55edccd67ab23bd9daee
BowieD <31174978+iBowie@users.noreply.github.com>
2020-11-02 22:08:44 +0500

    added moon speed controller

---
+++ Assets/Scripts/MoonSpeedController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoonSpeedController : MonoBehaviour
+{
+    public static MoonSpeedController Instance { get; private set; }
+
+    public float currentSpeed = 0f;
+    public float totalDistance = 0f;
+
+    public Text UIText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+    }
+
+    void FixedUpdate()
+    {
+        totalDistance += (currentSpeed / 50f);
+
+        UIText.text = $"Current Moon Speed: {currentSpeed:0.##} m/s\nTotal Distance: {totalDistance:0.##} meters";
+    }
+}
