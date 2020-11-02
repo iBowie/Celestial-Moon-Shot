3b34749e3584e410d7ed4b8cf0e5e0c40f8ee465
BowieD <31174978+iBowie@users.noreply.github.com>
2020-11-02 22:20:08 +0500

    better display of units

--- Assets/Scripts/MoonSpeedController.cs
+++ Assets/Scripts/MoonSpeedController.cs
@@ -11,6 +11,7 @@ public class MoonSpeedController : MonoBehaviour
     public float totalDistance = 0f;
 
     public Text UIText;
+    public ParticleSystem particles;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +23,34 @@ public class MoonSpeedController : MonoBehaviour
     {
         totalDistance += (currentSpeed / 50f);
 
-        UIText.text = $"Current Moon Speed: {currentSpeed:0.##} m/s\nTotal Distance: {totalDistance:0.##} meters";
+        toDisplay(totalDistance, out float disValue, out string disUnits);
+        toDisplay(currentSpeed, out float spdValue, out string spdUnits);
+
+        UIText.text = $"Current Moon Speed: {spdValue:0.##} {spdUnits}/s\nTotal Distance: {disValue:0.##} {disUnits}";
+
+        var particleArray = new ParticleSystem.Particle[0];
+
+        int l = particles.GetParticles(particleArray);
+
+        for (int i = 0; i < particleArray.Length; i++)
+        {
+            ParticleSystem.Particle p = particleArray[i];
+
+            p.velocity = new Vector3(0, -currentSpeed);
+        }
+    }
+
+    void toDisplay(float value, out float visibleValue, out string units)
+    {
+        if (value > 1000f)
+        {
+            visibleValue = value / 1000f;
+            units = "km";
+        }
+        else
+        {
+            visibleValue = value;
+            units = "m";
+        }
     }
 }
