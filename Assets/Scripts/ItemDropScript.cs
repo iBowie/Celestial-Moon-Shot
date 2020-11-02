ddd66510359abb5a45f0afd8d68785190361e831
BowieD <31174978+iBowie@users.noreply.github.com>
2020-11-02 21:39:02 +0500

    added item drops

---
+++ Assets/Scripts/ItemDropScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDropScript : MonoBehaviour
+{
+    public InventoryItemData itemData;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var inv = collision.gameObject.GetComponent<PlayerInventory>();
+        if (inv != null)
+        {
+            inv.items.Add(itemData);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    public List<InventoryItemData> items = new List<InventoryItemData>();
+}
+
+[Serializable]
+public class InventoryItemData
+{
+    public string name;
+    public GameObject prefab;
+}
