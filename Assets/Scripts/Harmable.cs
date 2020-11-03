a0811ffa25264aba3253f84e7ef1545b79dc4d36
BowieD <31174978+iBowie@users.noreply.github.com>
2020-11-03 12:55:43 +0500

    added health bars

--- Assets/Scripts/Harmable.cs
+++ Assets/Scripts/Harmable.cs
@@ -1,19 +1,32 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
+[ExecuteInEditMode]
 public class Harmable : MonoBehaviour
 {
     public float health;
     public float maxHealth;
 
+    public float healthPercentage => health / maxHealth;
+
+    public Image healthBarImage;
+
+    private float lastHealth, lastMaxHealth;
+
     private void Start()
     {
+        lastHealth = health;
+        lastMaxHealth = maxHealth;
+
         if (OnHarm == null)
             OnHarm = new UnityEvent<float>();
         
         if (OnDeath == null)
             OnDeath = new UnityEvent();
+
+        UpdateHealthBar();
     }
 
     public void Harm(float damage)
@@ -22,11 +35,43 @@ public class Harmable : MonoBehaviour
 
         health -= damage;
 
-        if (health < 0)
+        if (health <= 0)
         {
             OnDeath.Invoke();
 
             Destroy(this.gameObject);
+        }
+        else
+        {
+            UpdateHealthBar();
+        }
+    }
+
+    private void Update()
+    {
+        if (lastMaxHealth != maxHealth)
+        {
+            UpdateHealthBar();
+        }
+        if (lastHealth != health)
+        {
+            UpdateHealthBar();
+        }
+
+        lastMaxHealth = maxHealth;
+        lastHealth = health;
+    }
+
+    void UpdateHealthBar()
+    {
+        if (healthBarImage != null)
+        {
+            healthBarImage.fillAmount = healthPercentage;
+
+            float r = 1f - healthPercentage;
+            float g = healthPercentage;
+
+            healthBarImage.color = new Color(r, g, 0f);
         }
     }
 
