f48916f8a7221dd09af67226fbe940c324b82b00
BowieD <31174978+iBowie@users.noreply.github.com>
2020-11-04 13:26:21 +0500

    added speed effect on stars

--- Assets/Scripts/MoonSpeedController.cs
+++ Assets/Scripts/MoonSpeedController.cs
@@ -8,11 +8,13 @@ public class MoonSpeedController : MonoBehaviour
 {
     public static MoonSpeedController Instance { get; private set; }
 
+    private static float oldCurrentSpeed = 0f;
     public static float currentSpeed = 0f;
     public static float totalDistance = 0f;
 
     public Text UIText;
     public ParticleSystem particles;
+    public float editorSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -25,12 +27,25 @@ public class MoonSpeedController : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (Application.isEditor)
+        {
+            currentSpeed = editorSpeed;
+        }
+
         totalDistance += (currentSpeed / 50f);
 
         toDisplay(totalDistance, out float disValue, out string disUnits);
         toDisplay(currentSpeed, out float spdValue, out string spdUnits);
 
         UIText.text = $"Current Moon Speed: {spdValue:0.##} {spdUnits}/s\nTotal Distance: {disValue:0.##} {disUnits}";
+
+        if (oldCurrentSpeed != currentSpeed)
+        {
+            var ps = particles;
+            var v = ps.velocityOverLifetime;
+            v.speedModifierMultiplier = currentSpeed;
+        }
+        oldCurrentSpeed = currentSpeed;
     }
 
     public void GoToFinalScore()
