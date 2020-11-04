13b19572ff94a8a6ba07576a18fa74611462cb26
BowieD <31174978+iBowie@users.noreply.github.com>
2020-11-04 13:44:52 +0500

    added destroy out of bounds particles

---
+++ Assets/Scripts/DestroyParticlesOutOfBounds.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParticlesOutOfBounds : MonoBehaviour
+{
+    public Bounds bounds;
+    private ParticleSystem ps;
+    private ParticleSystem.Particle[] particles;
+    
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        if (ps == null)
+            throw new MissingComponentException("Missing ParticleSystem");
+        else
+            particles = new ParticleSystem.Particle[ps.main.maxParticles];
+    }
+
+    void FixedUpdate()
+    {
+        var count = ps.GetParticles(particles);
+        for (var i = 0; i < count; i++)
+        {
+            if (!bounds.Contains(particles[i].position))
+            {
+                particles[i].remainingLifetime = -1.0f;
+            }
+        }
+        ps.SetParticles(particles, count);
+    }
+}
