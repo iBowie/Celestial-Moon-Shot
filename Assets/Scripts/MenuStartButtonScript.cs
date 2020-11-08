30f45ce6b133267422e7b91ecd8475b1296edc21
BowieD <31174978+iBowie@users.noreply.github.com>
2020-11-09 00:25:52 +0500

    Update MenuStartButtonScript.cs

--- Assets/Scripts/MenuStartButtonScript.cs
+++ Assets/Scripts/MenuStartButtonScript.cs
@@ -5,6 +5,7 @@ public class MenuStartButtonScript : MonoBehaviour
 {
     private void OnMouseDown()
     {
+        SceneManager.LoadScene(1);
         var scene = SceneManager.GetSceneAt(1);
         SceneManager.SetActiveScene(scene);
     }
